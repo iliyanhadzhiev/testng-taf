4ebcf7d3d92e29046172a82a4556749ec7c64103
iliyanhadzhiev <iliqnh@gmail.com>
2024-04-02 17:14:29 +0300

    Implement CalcDivision + minor fixes

--- src/test/java/calculator/CalcDivision.java
+++ src/test/java/calculator/CalcDivision.java
@@ -1,4 +1,30 @@
 package calculator;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class CalcDivision {
+
+    @DataProvider(name = "numProvider")
+    public Object[][] numProvider() {
+        return new Object[][] {
+                {15, 3, 5},
+                {-2, 2, -1},
+                {20, -4, -5}
+        };
+    }
+
+    @Test(dataProvider = "numProvider", groups = "multiplication", suiteName = "CalculatorOperations")
+    public void validateNumDivision(double num1, double num2, double result) {
+
+        double firstProvidedNum = num1;
+        double secondProvidedNum = num2;
+        double expectedResult = result;
+        double actualResult = firstProvidedNum / secondProvidedNum;
+
+        System.out.println("The division of " + firstProvidedNum + " and " + secondProvidedNum + " is equal: " + actualResult);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 }
