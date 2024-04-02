3fe68840bfb39f6f588968281eafbb321a3e49f1
iliyanhadzhiev <iliqnh@gmail.com>
2024-04-02 15:12:33 +0300

    Edit Calculator.xml and implement addition

---
+++ src/test/java/calculator/CalcAddition.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CalcAddition {
+
+    @DataProvider(name = "numProvider")
+    public Object[][] numProvider() {
+        return new Object[][] {
+                {1, 1, 2},
+                {-2, 2, 0},
+                {19, -41, 22}
+        };
+    }
+
+    @Test(dataProvider = "numProvider", groups = "addition")
+    public void validateNumAddition(double num1, double num2, double expectedResult) {
+
+        double firstProvidedNum = num1;
+        double secondProvidedNum = num2;
+        double expectedAdditionResult = expectedResult;
+        double actualAdditionResult = firstProvidedNum + secondProvidedNum;
+
+        System.out.println("The addition of " + firstProvidedNum + " and " + secondProvidedNum + " is equal: " + actualAdditionResult);
+
+        Assert.assertEquals(expectedAdditionResult, actualAdditionResult);
+    }
+}
--- src/test/java/calculator/Main.java
+++ src/test/java/calculator/Main.java
@@ -2,5 +2,7 @@ package calculator;
 
 public class Main {
 
-    System.out.println("Hello");
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
