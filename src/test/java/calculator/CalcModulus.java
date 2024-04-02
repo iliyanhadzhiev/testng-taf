6a60b0d8b98fbf9dc910c0ba9e2fdb5ad71345c3
iliyanhadzhiev <iliqnh@gmail.com>
2024-04-02 17:21:02 +0300

    Implement CalcModulus

--- src/test/java/calculator/CalcModulus.java
+++ src/test/java/calculator/CalcModulus.java
@@ -1,4 +1,30 @@
 package calculator;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class CalcModulus {
+
+    @DataProvider(name = "numProvider")
+    public Object[][] numProvider() {
+        return new Object[][] {
+                {15, 4, 1},
+                {-2, 2, 0},
+                {20, -3, 2}
+        };
+    }
+
+    @Test(dataProvider = "numProvider", groups = "multiplication", suiteName = "CalculatorOperations")
+    public void validateNumModulus(double num1, double num2, double result) {
+
+        double firstProvidedNum = num1;
+        double secondProvidedNum = num2;
+        double expectedResult = result;
+        double actualResult = firstProvidedNum % secondProvidedNum;
+
+        System.out.println("The division of " + firstProvidedNum + " and " + secondProvidedNum + " is equal: " + actualResult);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 }
--- src/test/java/calculator/Main.java
+++ src/test/java/calculator/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         //for testing purposes:
-        double number1 = 5;
-        double number2 = 3;
+        double number1 = 20;
+        double number2 = -3;
         double calcResult = number1 % number2;
         System.out.println(calcResult);
     }
